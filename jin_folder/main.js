00ecc6c002df9ac4c807615fc03cd4931cd9fe81
Hwang-Sangjin <aosdb5640@naver.com>
2021-07-17 22:13:27 +0900

    filelist

--- jin_folder/main.js
+++ jin_folder/main.js
@@ -5,39 +5,84 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url,true).query;
+    var pathname = url.parse(_url, true).pathname;
     var title = queryData.id;
-    if(_url == '/'){
-      title = "Welcome";
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
+    
+    if(pathname === '/'){
+      if(title === undefined){
 
-    fs.readFile(`data/${title}`,'utf8', function (err,data){
-      var description = data;
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}
-        </p>
-      </body>
-      </html>
-      `;
-      response.end(template);
-    })
+        fs.readdir('./data', function(err, filelist){
+          title = 'Welcome';
+          var description = 'Hello, Node.js';
+
+          var list = '<ul>';
+          for(var i=0;i<filelist.length;i++){
+            list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+          }
+          list += '</ul>'
+
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}
+            </p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200); // 파일을 성공적으로 전달
+          response.end(template);
+        })
+
+        
+      }
+      else{
+        fs.readdir('./data', function(err, filelist){
+          title = queryData.id;
+          var list = '<ul>';
+          for(var i=0;i<filelist.length;i++){
+            list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+          }
+          list += '</ul>'
+
+          fs.readFile(`data/${title}`,'utf8', function (err,description){
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>${description}
+              </p>
+            </body>
+            </html>
+            `;
+            response.writeHead(200); // 파일을 성공적으로 전달
+            response.end(template);
+          });
+        });
+      }
+
+      
+    }
+    else{
+      response.writeHead(404);// 파일을 전달을 실패
+      response.end("Not found");
+    }
+
+    
 
     
 
