d2e696c5d3dc924c78a04c5673f029b9064c9112
Hwang-Sangjin <aosdb5640@naver.com>
2021-07-17 16:18:05 +0900

    template literal

---
+++ jin_folder/syntax/template.js
@@ -0,0 +1,12 @@
+var name = "jin";
+
+var letter = `Dear ${name}
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin volutpat, felis nec lobortis vulputate, orci dolor pulvinar quam, sit amet dapibus felis ante aliquam ante. Sed tempor felis sem, et ullamcorper mi suscipit et. Vestibulum eget fringilla ante, ut luctus arcu. Fusce quis dapibus erat. Suspendisse ultrices vitae diam et sollicitudin. Morbi sit amet tellus neque. Etiam volutpat, tortor vitae posuere imperdiet, lorem ante vulputate ante, vel tempus risus velit et orci. Duis elit quam, consequat at hendrerit quis, tempor nec tellus. Ut commodo hendrerit lorem, sit amet feugiat libero consequat a. Quisque id dictum ex. Curabitur non convallis nisl, a convallis nisl.`;
+
+//console.log(letter);
+
+var a = 5;
+var b = 10;
+
+console.log(`a + b = ${a+b}`);
