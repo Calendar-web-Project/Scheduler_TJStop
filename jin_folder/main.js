9c81c07c87516d63c62cba81b5a9c93333b99290
Hwang-Sangjin <aosdb5640@naver.com>
2021-07-20 20:53:10 +0900

    update done

--- jin_folder/main.js
+++ jin_folder/main.js
@@ -83,7 +83,7 @@ var app = http.createServer(function(request,response){
 
         var template = templateHTML(title, list,
           `
-          <form action= "http://localhost:3000/create_process" method="post">
+          <form action= "/create_process" method="post">
             <p><input type = "text" name = "title" placeholder = "title"></p>
             <p>
               <textarea name = "description" placeholder = "description"></textarea>
@@ -110,6 +110,51 @@ var app = http.createServer(function(request,response){
         fs.writeFile(`data/${title}`, description, 'utf8', function(err){
           response.writeHead(302, {Location: `/?id=${title}`});
           response.end('success');
+        })
+      }); 
+    }
+    else if(pathname === "/update"){
+      fs.readdir('./data', function(err, filelist){
+        title = queryData.id;
+
+        fs.readFile(`data/${title}`,'utf8', function (err,description){
+          var list = templatelist(filelist);
+          var template =templateHTML(title, list,
+            `
+            <form action= "/update_process" method="post">
+              <input type = "hidden" name = "id" value = "${title}">
+              <p><input type = "text" name = "title" placeholder = "title" value ="${title}"></p>
+              <p>
+                <textarea name = "description" placeholder = "description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+            `,
+            `<a href="/create"> create </a> <a href="/update?id=${title}"> update </a>`
+            );
+          
+          response.writeHead(200); // 파일을 성공적으로 전달
+          response.end(template);
+        });
+      });
+    }
+    else if(pathname ==="/update_process"){
+      var body ="";
+      request.on("data", function(data) {
+        body+=data;
+      });
+      request.on("end", function() {
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+        fs.rename(`data/${id}`, `data/${title}`, function(err){
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+            response.writeHead(302, {Location: `/?id=${title}`});
+            response.end('success');
+          })
         })
       }); 
     }
