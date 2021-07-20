9b916cab5d1194b7856b3c0030d338f389d5c96e
Hwang-Sangjin <aosdb5640@naver.com>
2021-07-20 16:07:05 +0900

    create update

--- jin_folder/main.js
+++ jin_folder/main.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
   return `
   <!doctype html>
   <html>
@@ -11,9 +12,9 @@ function templateHTML(title, list, body){
     <meta charset="utf-8">
   </head>
   <body>
-    <h1><a href="/">WEB2</a></h1>
+    <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href="/create"> create </a>
+    ${control}
     ${body}
   </body>
   </html>
@@ -44,7 +45,10 @@ var app = http.createServer(function(request,response){
 
           var list = templatelist(filelist);
 
-          var template = templateHTML(title, list,`<h2>${title}</h2>${description}`);
+          var template = templateHTML(title, list,
+            `<h2>${title}</h2>${description}`, 
+            `<a href="/create"> create </a>`
+            );
           response.writeHead(200); // 파일을 성공적으로 전달
           response.end(template);
         })
@@ -57,7 +61,10 @@ var app = http.createServer(function(request,response){
 
           fs.readFile(`data/${title}`,'utf8', function (err,description){
             var list = templatelist(filelist);
-            var template =templateHTML(title, list,`<h2>${title}</h2>${description}`);
+            var template =templateHTML(title, list,
+              `<h2>${title}</h2>${description}`,
+              `<a href="/create"> create </a> <a href="/update?id=${title}"> update </a>`
+              );
             
             response.writeHead(200); // 파일을 성공적으로 전달
             response.end(template);
@@ -66,6 +73,45 @@ var app = http.createServer(function(request,response){
       }
 
       
+    }
+    else if(pathname === "/create"){
+      fs.readdir('./data', function(err, filelist){
+        title = 'WEB - create';
+        var description = 'Hello, Node.js';
+
+        var list = templatelist(filelist);
+
+        var template = templateHTML(title, list,
+          `
+          <form action= "http://localhost:3000/create_process" method="post">
+            <p><input type = "text" name = "title" placeholder = "title"></p>
+            <p>
+              <textarea name = "description" placeholder = "description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+          `, ``
+        );
+        response.writeHead(200); // 파일을 성공적으로 전달
+        response.end(template);
+      });
+    }
+    else if(pathname === "/create_process"){
+      var body ="";
+      request.on("data", function(data) {
+        body+=data;
+      });
+      request.on("end", function() {
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          response.writeHead(302, {Location: `/?id=${title}`});
+          response.end('success');
+        })
+      }); 
     }
     else{
       response.writeHead(404);// 파일을 전달을 실패
