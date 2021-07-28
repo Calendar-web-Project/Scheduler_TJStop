297451a01e9e3d1e4c8122af592c3e6b2e1baa15
Hwang-Sangjin <52124805+Hwang-Sangjin@users.noreply.github.com>
2021-07-29 01:33:42 +0900

    calendar

---
+++ jin_folder/main.js
@@ -0,0 +1,81 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+function templateHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+
+function templateList
+
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url,true).query;
+    var pathname = url.parse(_url, true).pathname;
+    var title = queryData.id;
+    
+    if(pathname === '/'){
+      if(title === undefined){
+
+        fs.readdir('./data', function(err, filelist){
+          title = 'Welcome';
+          var description = 'Hello, Node.js';
+
+          var list = '<ul>';
+          for(var i=0;i<filelist.length;i++){
+            list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+          }
+          list += '</ul>'
+
+          var template = templateHTML(title, list,`<h2>${title}</h2>${description}`);
+          response.writeHead(200); // 파일을 성공적으로 전달
+          response.end(template);
+        })
+
+        
+      }
+      else{
+        fs.readdir('./data', function(err, filelist){
+          title = queryData.id;
+          var list = '<ul>';
+          for(var i=0;i<filelist.length;i++){
+            list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+          }
+          list += '</ul>'
+
+          fs.readFile(`data/${title}`,'utf8', function (err,description){
+            var template =templateHTML(title, list,`<h2>${title}</h2>${description}`);
+            response.writeHead(200); // 파일을 성공적으로 전달
+            response.end(template);
+          });
+        });
+      }
+
+      
+    }
+    else{
+      response.writeHead(404);// 파일을 전달을 실패
+      response.end("Not found");
+    }
+
+    
+
+    
+
+    
+ 
+});
+app.listen(3000);
